557f3fb328276270981e728357363592aba0e8cd
Daniel Kelly <me@danielkelly.io>
2024-12-04 07:43:19 -0600

    3-slots-and-template-props-pattern

---
+++ components/AppButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    // state props
+    loading: { type: Boolean, default: false },
+
+    // configuration props
+    outline: { type: Boolean, default: false },
+})
+
+</script>
+<template>
+    <button class="btn flex items-center" :class="{
+        'btn-outline': outline
+    }">
+        <AppSpinner v-if="loading" />
+        <slot></slot>
+    </button>
+</template>
--- pages/3-slots-and-template-props.vue
+++ pages/3-slots-and-template-props.vue
@@ -1,9 +1,20 @@
-<script setup>
+<script setup lang="ts">
+const loading = ref(false);
+watch(loading, async () => {
+    if (loading.value) {
+        await sleep(2000);
+        loading.value = false
+    }
 
+})
 </script>
-
 <template>
     <div>
-
+        <PageHeading :number="3">Slots and Template Props Pattern</PageHeading>
+        <div class="flex gap-3">
+            <AppButton :outline="true" @click="loading = true" :loading="loading">
+                <strong>Click Me</strong>
+            </AppButton>
+        </div>
     </div>
 </template>
