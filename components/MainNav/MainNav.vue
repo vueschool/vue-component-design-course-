92c79b9e84c08a792d9ba660253101af3b9d61f0
Daniel Kelly <me@danielkelly.io>
2024-12-04 10:47:26 -0600

    chore: refactor main nav

---
+++ components/MainNav/MainNav.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="drawer">
+        <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col">
+            <!-- Navbar -->
+            <div class="navbar bg-base-300 w-full">
+                <div>
+                    <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            class="inline-block h-6 w-6 stroke-current">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </label>
+                </div>
+                <div class="mx-2 flex-1 px-2">Vue Component Design Course</div>
+
+            </div>
+            <div class="container mx-auto py-5">
+                <slot></slot>
+            </div>
+
+        </div>
+        <div class="drawer-side">
+            <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
+            <MainNavLinksList />
+        </div>
+    </div>
+</template>
---
+++ components/MainNav/MainNavLinksList.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const links = [
+    {
+        title: 'Branching Component Pattern',
+        to: '2-branching-component-pattern',
+    },
+    {
+        title: 'Slots and Template Props Pattern',
+        to: '3-slots-and-template-props',
+    },
+    {
+        title: 'List with ListItem Pattern',
+        to: '4-list-with-list-item-pattern',
+    },
+    {
+        title: 'Smart vs Dumb Components Pattern',
+        to: '5-smart-vs-dumb-components-pattern',
+    },
+    {
+        title: "Form Component Pattern",
+        to: "6-form-component-pattern",
+    },
+    {
+        title: "A Few Advanced Vue Component Patterns",
+        to: "7-a-few-advanced-vue-component-patterns",
+    }
+];
+</script>
+<template>
+
+    <ul class="menu bg-base-200 min-h-full w-80 p-4">
+        <!-- Sidebar content here -->
+        <li v-for="link in links">
+            <NuxtLink :to="link.to">{{ link.title }}</NuxtLink>
+        </li>
+    </ul>
+</template>
